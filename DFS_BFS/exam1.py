c41723e0e7de8e097ddbeae2605702d1924f58f4
Yoon <tkddnjs1648@gmail.com>
2021-10-26 18:43:09 +0900

    Queue 이해

--- DFS_BFS/exam1.py
+++ DFS_BFS/exam1.py
@@ -1,1 +1,26 @@
-exam1
+# 큐(Queue) 예제
+
+from collections import deque
+queue = deque()
+
+queue.append(5)
+print(queue)
+queue.append(2)
+print(queue)
+queue.append(3)
+print(queue)
+queue.append(7)
+print(queue)
+queue.popleft()
+print(queue)
+queue.append(1)
+print(queue)
+queue.append(4)
+print(queue)
+queue.popleft()
+
+# 먼저 들어온 순서대로 출력
+print(queue)
+# 나중에 들어온 순서대로 출력
+queue.reverse()
+print(queue)
