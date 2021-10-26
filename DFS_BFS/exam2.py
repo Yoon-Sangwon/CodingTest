7e2a68953c22f26bc1a5555f809ed91b28913ba8
Yoon <tkddnjs1648@gmail.com>
2021-10-26 18:51:48 +0900

    재귀함수 응용

--- DFS_BFS/exam2.py
+++ DFS_BFS/exam2.py
@@ -1,6 +1,16 @@
 # 재귀 함수
+
+'''
 def recursive_function():
     print("재귀함수 호출")
     recursive_function()
 
 recursive_function()
+'''
+
+def factorial(n):
+    if n <= 1:
+        return 1  # 종료조건 작성 매우 중요!
+    return n * factorial(n-1)
+
+print(factorial(5))
